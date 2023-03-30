eafcae5231d66c3bf641d2407ef7de131f06542e
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-03-30 18:05:58 +0300

    Minor changes

--- additional.py
+++ additional.py
@@ -1,21 +1,20 @@
 import requests
 
+
 def get_flights_with_photo_ids():
 
     """Сохраняет список id полетов с датами и количестом фотографий"""
 
-    response = requests.get(f'https://api.spacexdata.com/v5/launches/')
-    spaceX_data = response.json()
+    response = requests.get('https://api.spacexdata.com/v5/launches/')
+    spacex_data = response.json()
 
     ids = []
-    for flight in spaceX_data:
+    for flight in spacex_data:
         if flight['links']['flickr']['original']:
             ids.append("-".join([flight['id'],
                                  flight['date_local'],
                                  str(len(flight['links']['flickr']['original'])),
-                                 ]
-                                )
-                       )
+                                 ]))
 
     with open('launches.txt', 'w') as file:
         file.write(" ".join(ids))
