b52dcd51442b35d42ebd890c3a14a28f9c4be796
Nataliya Kashaeva <65728137+kashaeva-dev@users.noreply.github.com>
2023-04-04 21:28:31 +0300

    Delete send_message() function

--- bot.py
+++ bot.py
@@ -1,11 +1,6 @@
 import telegram
 
 
-def send_message(text, chat_id, token):
-    bot = telegram.Bot(token)
-    bot.send_message(text=text, chat_id=chat_id)
-
-
 def send_image(path, chat_id, token):
     bot = telegram.Bot(token=token)
     with open(path, 'rb') as image:
