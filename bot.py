dfd2d9885043c999f09971389134b4384eec04c4
Nataliya Kashaeva <65728137+kashaeva-dev@users.noreply.github.com>
2023-04-04 21:25:48 +0300

    Replace 'open()' with 'with open()'

--- bot.py
+++ bot.py
@@ -8,4 +8,5 @@ def send_message(text, chat_id, token):
 
 def send_image(path, chat_id, token):
     bot = telegram.Bot(token=token)
-    bot.send_document(chat_id=chat_id, document=open(path, 'rb'), timeout=100)
+    with open(path, 'rb') as image:
+        bot.send_document(chat_id=chat_id, document=image, timeout=100)
